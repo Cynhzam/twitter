83a00a7d78cb86bb1b3dba36aea421f8a8c34a14
Cynhzam <cyn.herrerazamudio@gmail.com>
2016-09-22 10:49:21 -0500

    subiendo versiones extras

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ window.addEventListener("load", function() {
     button.addEventListener("click", function(e) { 
        e.preventDefault();
 	function myFunction() {
-        var texto = document.getElementById("texto").value;
         var newItem = document.createElement("p");
-        newItem.appendChild(textnode);
+        var textoo = document.getElementById("texto").value;
+        var texto = document.createTextNode(textoo);
+        newItem.appendChild(texto);
         var resultado = document.getElementById("resultado");
         resultado.insertBefore(newItem,resultado.childNodes[0]);
+        var fecha = new Date();
+        var hora = fecha.getHours();
+        var minuto = fecha.getMinutes();
+        var contenedorHora = document.createElement("span");
+        contenedorHora.innerText = hora + ":" + minuto;
+        newItem.insertBefore(contenedorHora,newItem.childNodes[0]);
         }
         myFunction();
 	}); 
@@ -23,7 +30,7 @@ window.addEventListener("load", function() {
                 button.disabled = true;
             } else if(tamaño > maximo) {
                 button.disabled = true;
-            } else if(tamaño > 120){
+            } else if(tamaño > 120) {
                 conteo.classList.add("crimson");
             } else {
                 conteo.classList.remove("crimson");
@@ -41,6 +48,12 @@ window.addEventListener("load", function() {
             texto.rows -= 1; 
         } if(texto.rows < 3) {
             texto.rows == 2;
+        }
+    }
+    function validarBox(texto) {
+        var texto = document.getElementById("texto");
+        if (texto.value.length > texto.rows * texto.cols) {
+            texto.rows += 1; 
         }
     }
 });
